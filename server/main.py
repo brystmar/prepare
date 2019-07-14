198c76e8b8d9b7ae9235dc9f41b1786bbcfe7216
Stephen Chan <stephen.s.c.chan@gmail.com>
2019-07-14 06:44:37 -0700

    Integrated Evacuation Advice API

--- server/main.py
+++ server/main.py
@@ -40,3 +40,20 @@ def generate_id_and_put_public_user_data():
     put_public_user_data(new_random_user_id)
     return str(uuid.uuid4())
 
+@app.route('/evacadvice/', methods=["GET"])
+def get_evac_advice():
+    print("get_evac_advice_called, request.args is:")
+    print(request.args);
+    advice_map = {'421 Harmony Street New Orleans, LA 70115': 'SHELTER IN PLACE',
+                  '711 Congress St, New Orleans, LA 70117': 'EVACUATE NOW',
+                  '140 Oakmont Dr, New Orleans, LA 70128': 'PREPARE TO EVACUATE',
+                  '937 France St, New Orleans, LA 70117': 'YOU ARE SAFE'}
+    if request.args is None or request.args.get("address") is None:
+        advice = None
+    else:
+        advice = advice_map.get(request.args.get("address"))
+
+    if advice == None:
+        return "PREPARE TO EVACUATE"
+    else:
+        return advice
